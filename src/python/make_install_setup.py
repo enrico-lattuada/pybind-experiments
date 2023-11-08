37571568e488108e72f007badaa488c3846934cb
Enrico Lattuada <lattuada.enrico@gmail.com>
2023-11-08 20:53:13 +0100

    DEV: Add make_install_setup

---
+++ src/python/make_install_setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+import sys
+
+if sys.version_info < (3, 6):
+    sys.exit("Error, Python < 3.6 is not supported.")
+
+setup(
+    name="pybind_experiments",
+    use_scm_version={
+        "root": "${CMAKE_SOURCE_DIR}",
+        "fallback_version": "0.3.0",
+    },
+    setup_requires=["setuptools_scm"],
+    packages=["pybind_experiments"],
+    package_dir={"pybind_experiments": "${PYBINDEXP_OUTPUT_DIR}"},
+    package_data={"pybind_experiments": ["_core.so"]},
+)
